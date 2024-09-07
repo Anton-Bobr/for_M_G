235b74cde6c1656f4dfeeac0b449362a44ce2081
Anton_Piatrouski <antonpiatrouski@gmail.com>
2024-09-07 18:30:23 +0300

    main fix jenkins.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -41,7 +41,8 @@ def getProject(String repo, String branch) {
 
 def runTestWithTag(String tag) {
     try {
-        labelledShell(label: "Run ${tag}", script: "chmod +x gradlew \n./gradlew -x test ${tag}")
+        labelledShell(label: "Run ${tag}", script: "./gradlew -x test ${tag}")
+//        labelledShell(label: "Run ${tag}", script: "chmod +x gradlew \n./gradlew -x test ${tag}")
     } finally {
         echo "some failed tests"
     }
