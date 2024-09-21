a463d944a671ec3e4be3a6bffdc2096a06127bc7
Java_Service <antonpiatrouski@gmail.com>
2024-09-21 21:16:04 +0300

    main fix jenkins.groovy

--- src/main/java/com/manyvids/parser/selenium/page/MainPage.java
+++ src/main/java/com/manyvids/parser/selenium/page/MainPage.java
@@ -15,7 +15,7 @@ public class MainPage extends AbstractPage {
 
     private final static String PAGE_URN = "";
     private final static String CREATORS_ICON_XPATH =
-        "//div[contains(@class, 'smallSideBar_small_sidebar')]//a[@data-event-label='Creators sss']";
+        "//div[contains(@class, 'smallSideBar_small_sidebar')]//a[@data-event-label='Creators']";
 
     public MainPage(final WebDriver driver) {
         super(driver);
